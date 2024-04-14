23d6bad51cc54f027e68e724cf6a2450df63d954
Tilak Singh <tsingh99991@gmail.com>
2024-04-14 09:11:23 +0530

    Dom Updated

--- RandomColor/src/components/RandomColor.jsx
+++ RandomColor/src/components/RandomColor.jsx
@@ -1,29 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function RandomColor() {
-  const [color, setColor] = useState("#000000");
+  const [color, setColor] = useState("#aaa");
   const [typeOf, setTypeOf] = useState("hex");
+
+  useEffect(() => {
+    if (typeOf === "hex") {
+      handleOnHexColor();
+    } else {
+      handleOnRGBColor();
+    }
+  }, [typeOf]);
 
   function randomColor(length) {
     return Math.floor(Math.random() * length);
   }
   const handleOnHexColor = () => {
     //example #000000
-    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "G", "F"];
+    const hex = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "G", "F"];
     let hexColor = "#";
 
     for (let i = 0; i < 6; i++) {
       hexColor += hex[randomColor(hex.length)];
     }
     // console.log(hexColor);
-    setColor(hexColor)
+    setColor(hexColor);
   };
   const handleOnRGBColor = () => {
     const r = randomColor(255);
     const g = randomColor(255);
     const b = randomColor(255);
 
-    setColor(`rgb(${r},${g},${b})`)
+    setColor(`rgb(${r},${g},${b})`);
   };
   return (
     <div>
@@ -55,7 +63,11 @@ function RandomColor() {
           width: "200px",
           backgroundColor: color,
         }}
-      ></div>
+      >
+        <b>{typeOf === "rgb" ? "RGB color" : "HEX color"}</b>
+        <br />
+        <p>{color}</p>
+      </div>
     </div>
   );
 }
