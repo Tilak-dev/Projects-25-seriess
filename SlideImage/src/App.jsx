fc50ff69e4f8a336c01af6b2a57d2dbcab27c154
Tilak Singh <tsingh99991@gmail.com>
2024-04-26 12:41:34 +0530

    carousel image slider project init

---
+++ SlideImage/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Stars from './components/Stars'
+
+function App() {
+  return (
+    <div >
+      <Stars indexOfStars={10}/>
+    </div>
+  )
+}
+
+export default App
